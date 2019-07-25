9b1071269d4362a095b1dfc75c0145182d8a7a0a
Vyacheslav Dorzhiev <aliencrowdman@yandex.ru>
2019-07-26 02:35:30 +0300

    add solution

--- src/run.ts
+++ src/run.ts
@@ -4,7 +4,6 @@ import ITask from './Task';
 export default async function run(executor: IExecutor, queue: Iterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
     /**
-     * Код надо писать сюда
-     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
+     * Решение было тут!
      */
 }
